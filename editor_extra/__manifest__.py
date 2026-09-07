1c84b6e1d9fe051a4d53c2909eb97eb43da8d440
Benoit Socias (bso) <bso@odoo.com>
2026-09-07 16:18:54 +0200

    oxp26 editor_extra: implicit conversion to kbd elements

---
+++ editor_extra/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    'name': 'Editor Extra',
+    'category': 'Example',
+    'sequence': 1,
+    'website': 'https://www.odoo.com/event/odoo-experience-2026-9099/track/into-the-odoo-editor-11015',
+    'summary': 'Explains how to add features to Odoo Editor',
+    'version': '1.0',
+    'depends': ['html_editor'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'application': True,
+    'installable': True,
+    'assets': {
+        'html_editor.assets_editor': [
+            'editor_extra/static/src/plugins/**/*',
+            'editor_extra/static/src/plugin_sets.js',
+        ],
+    },
+    'author': 'Odoo S.A.',
+    'license': 'LGPL-3',
+}
---
+++ editor_extra/static/src/plugin_sets.js
@@ -0,0 +1,4 @@
+import { MAIN_PLUGINS } from "@html_editor/plugin_sets";
+import { KeyPlugin } from "./plugins/key_plugin";
+
+MAIN_PLUGINS.push(KeyPlugin);
